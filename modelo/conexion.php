ad03e3a23c4c87afc8508c39606fbcb969fb5b3e
JABV75 <93306875+JABV75@users.noreply.github.com>
2022-11-28 20:28:42 -0600

    update

--- modelo/conexion.php
+++ modelo/conexion.php
@@ -3,7 +3,7 @@
     {
         static public function conectar()
         {
-            $link = new PDO("mysql:host=localhost;dbname=curso1","root","");
+            $link = new PDO("mysql:host=localhost;dbname=mydb","root","");
             $link->exec("set names utf8");
             return $link;
         }
